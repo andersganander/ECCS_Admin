ed41a0f5b4b10e65c9902c263af4ac942acc1703
Anders Ganander <anders.ganander@gmail.com>
2024-04-25 16:51:02 +0000

    Add empty funcions to be called from the main loop #2

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+# Global constants
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
@@ -20,5 +21,48 @@ print(data)
 # \n is needed for getting it to work in Heroku
 # data_str = input("Please enter:\n")
 
+def main():
+    pass
 
+def register_price():
+    """
+    Shows dialog and recieve users input (month and price)
+    """
+    pass
+# end def
 
+def create_report():
+    """
+    Shows dialog an recieves users input (month)
+    """
+    pass
+# end def
+
+def erase_month():
+    """
+    Shows dialog where user choose which months data to erase 
+    """
+    pass
+# end def
+
+def show_status():
+    """
+    Returns the status for all months
+    (month, price, report, date)
+    """
+    pass
+# end def
+
+def show_help():
+    """
+    Shows instructions for how to use the system
+    """
+    pass
+# end def
+
+def exit():
+    """
+    Exits the application 
+    """
+    pass
+# end def
